627f7d5b2b06e1c63339ac32fd606050a0b2017e
Tariq Salman <tariq@mashvisor.com>
2022-12-04 04:42:44 +0200

    style

--- src/GeoChart.js
+++ src/GeoChart.js
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect, useState } from "react";
 import { select, geoPath, geoAlbersUsa, min, max, scaleLinear } from "d3";
 import useResizeObserver from "./useResizeObserver";
-import cities from "./us_cities.geo.json"
+import mac from './mac.json';
 /**
  * Component that renders a map of Germany.
  */
@@ -13,6 +13,11 @@ function GeoChart({ data, starbucks, county }) {
   const [selectedState, setSelectedState] = useState(null);
   const [selectedCounty, setSelectedCounty] = useState(null);
   // will be called initially and on every data change
+  const [isMac, setMac] = useState(false);
+  const [isStar, setStar] = useState(true);
+  const [r, setR] = useState(5);
+
+
   const zoomOut = () => {
     if (selectedCounty) {
       setSelectedCounty(null)
@@ -22,10 +27,11 @@ function GeoChart({ data, starbucks, county }) {
     }
   }
   useEffect(() => {
-    console.log(selectedState)
     const svg = select(svgRef.current);
     svg.selectAll(".starbucks").remove();
     svg.selectAll(".county").remove();
+    svg.selectAll(".mac").remove();
+
 
     // use resized dimensions
     // but fall back to getBoundingClientRect, if no dimensions yet.
@@ -36,7 +42,8 @@ function GeoChart({ data, starbucks, county }) {
 
     const projection = geoAlbersUsa()
       .fitSize([width, height], selectedCounty || selectedState || data)
-      .precision(100);
+      .precision(100)
+   
 
     // takes geojson data,
     // transforms that into the d attribute of a path element
@@ -48,13 +55,13 @@ function GeoChart({ data, starbucks, county }) {
       .data(data.features)
       .join("path")
       .on('click', feature => {
+        console.log(feature)
         setSelectedState(feature.target.__data__)
       })
       .attr("class", "state")
       .transition()
       .attr("d", feature => pathGenerator(feature));
-    const points =
-      starbucks;
+
 
     if (selectedState) {
       const stateCounties = county.features.filter((co) => co.properties.COUNTY_STATE_NAME.includes(selectedState.properties.NAME))
@@ -62,8 +69,8 @@ function GeoChart({ data, starbucks, county }) {
         .data(stateCounties)
         .join("path").
         on('click', feature => {
-          console.log(feature)
           setSelectedCounty(feature.target.__data__)
+       
         })
         .attr("class", "county")
         .transition()
@@ -71,13 +78,26 @@ function GeoChart({ data, starbucks, county }) {
 
     }
 
-    console.log(points)
-    if (points) {
+if(isMac)
+    svg.selectAll(".mac")
+      .data(mac)
+      .enter().append("circle")
+      .attr("class", "mac")
+      .attr("r", r)
+      .attr("cx", function (d) {
+        var coords = projection([d.lng, d.lat])
+        return coords[0]
+      })
+      .attr("cy", function (d) {
+        var coords = projection([d.lng, d.lat])
+        return coords[1]
+      })
+      if(isStar)
       svg.selectAll(".starbucks")
-        .data(points)
+        .data(starbucks)
         .enter().append("circle")
         .attr("class", "starbucks")
-        .attr("r", 5)
+        .attr("r", r)
         .attr("cx", function (d) {
           var coords = projection([d.position.lng, d.position.lat])
           return coords[0]
@@ -86,12 +106,24 @@ function GeoChart({ data, starbucks, county }) {
           var coords = projection([d.position.lng, d.position.lat])
           return coords[1]
         })
-    }
-  }, [data, dimensions, starbucks, selectedState, selectedCounty]);
+
+  }, [data, dimensions, starbucks, selectedState, selectedCounty, mac,isMac,isStar,r]);
 
   return (
     <>
       <button onClick={zoomOut}>zoom out </button>
+      <div style={{display:"flex", justifyContent:"center"}}>
+        <input type="checkbox" id="mac" name="mac" onClick={()=>{setMac(m=>!m)}}/>
+        <label for="mac" > show mac</label>
+      </div>
+      <div style={{display:"flex", justifyContent:"center"}}>
+
+        <input checked={isStar} type="checkbox" id="star" name="star" onClick={()=>{setStar(m=>!m)}}/>
+        <label for="star"> show starbucks</label>
+      </div>
+    point radius
+      <input placeholder="point radius" defaultValue={5} onChange={(v)=>{setR(parseFloat(v.target.value))}}/>
+
       <div ref={wrapperRef} style={{ marginBottom: "2rem", width: "100%", height: "600px" }}>
         <svg ref={svgRef}></svg>
       </div>
